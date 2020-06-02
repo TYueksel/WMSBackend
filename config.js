785b511e7953d7b56b998842483d7556af960250
TYueksel <tolu95@web.de>
2020-06-03 00:46:30 +0530

    Added sensors and actuators

--- config.js
+++ config.js
@@ -1,9 +1,35 @@
 (function() {
 	
 	/**
-	 * TODO: Config for GPIO Pin for each sensor and actuator
+	 * TODO: Config for pins/channels of each sensor and actuator
 	 */
-	global.level_PIN = 5;
+	global.levelSensor1 = {
+		channel: 0,
+		value: null
+	};
+	global.levelSensor2 = {
+		channel: 1,
+		value: null
+	};
+	global.levelSensor3 = 8;
+	global.levelSensor4 = 8;
+	global.flowSensor = 21;
+	global.valve1 = {
+		PIN: 17,
+		state: 'off'
+	};
+	global.valve2 = {
+		PIN: 27,
+		state: 'off'
+	};
+	global.pump1 = {
+		PIN: 23,
+		state: 'off'
+	};
+	global.pump2 = {
+		PIN: 24,
+		state: 'off'
+	};
 	
 	/**
 	 * TODO: Config for local MySQL DB
