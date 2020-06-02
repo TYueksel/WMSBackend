d9f371fd5162a696eb61b4f25ad5d1df7af28d6a
TYueksel <tolu95@web.de>
2020-06-03 01:00:41 +0530

    changed lib and added some functions

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ require('./config.js');
 // connect to local db
 const db = require('./db.js');
 
+const spi = require('./spi.js');
+
 // Raspberry Pi GPIO
-require('./gpio.js');
+// require('./gpio.js');
 
 db.getBuildingIDs(function(res) {
 	console.log(res);
 });
+
+setInterval(() => {
+	spi.getWaterLevel(1);
+}, 2000)
